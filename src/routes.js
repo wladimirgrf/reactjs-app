df09a6c74a4f215fe90aa5469f54e76fe358c68f
Wladimir <wladimirgrf@gmail.com>
2020-02-24 20:31:59 -0300

    Repository params, Link component

--- src/routes.js
+++ src/routes.js
@@ -9,7 +9,7 @@ function Routes() {
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Main} />
-        <Route path="/repository" exact component={Repository} />
+        <Route path="/repository/:repository" exact component={Repository} />
       </Switch>
     </BrowserRouter>
   );
