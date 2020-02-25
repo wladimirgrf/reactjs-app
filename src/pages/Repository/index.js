102885a6b10dca3e971ec0b6811c26d1886bc2b9
Wladimir <wladimirgrf@gmail.com>
2020-02-24 22:31:37 -0300

    Issue List, Repository styles

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -4,17 +4,17 @@ import PropTypes from 'prop-types';
 
 import api from '../../services/api';
 import Container from '../../components/Container/index';
-import { Loading, Owner } from './styles';
-
-const propTypes = {
-  match: PropTypes.shape({
-    params: PropTypes.shape({
-      repository: PropTypes.string,
-    }),
-  }).isRequired,
-};
+import { Loading, Owner, IssueList } from './styles';
 
 class Repository extends Component {
+  static propTypes = {
+    match: PropTypes.shape({
+      params: PropTypes.shape({
+        repository: PropTypes.string,
+      }),
+    }).isRequired,
+  };
+
   constructor() {
     super().state = {
       repository: {},
@@ -60,6 +60,23 @@ class Repository extends Component {
           <h1>{repository.name}</h1>
           <p>{repository.description}</p>
         </Owner>
+
+        <IssueList>
+          {issues.map(issue => (
+            <li key={String(issue.id)}>
+              <img src={issue.user.avatar_url} alt={issue.user.login} />
+              <div>
+                <strong>
+                  <a href={issue.html_url}>{issue.title}</a>
+                  {issue.labels.map(label => (
+                    <span key={String(label.id)}>{label.name}</span>
+                  ))}
+                </strong>
+                <p>{issue.user.login}</p>
+              </div>
+            </li>
+          ))}
+        </IssueList>
       </Container>
     );
   }
